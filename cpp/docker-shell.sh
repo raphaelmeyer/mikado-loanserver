7b240a18e3a79c00d69f38da08baa0df8e453be4
raphael meyer <raphael.akira.meyer@gmail.com>
2018-06-23 07:31:15 +0200

    build with docker

---
+++ cpp/docker-shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if ! docker volume ls -q | grep -q mikado-workspace ; then
+  docker volume create mikado-workspace
+fi
+
+WORKSPACE="--mount source=mikado-workspace,target=/workspace"
+SOURCE="--mount type=bind,source=${script_dir},target=/source,readonly"
+VOLUMES="${WORKSPACE} ${SOURCE}"
+
+docker run --rm -it ${VOLUMES} \
+  -w /workspace \
+  raphaelmeyer/cmake3.10-clang6.0-runner:1.1.0
+
